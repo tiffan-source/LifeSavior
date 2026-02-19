8d981d7f6c05cb22682b57876429e985ff860491
tiffan-source <zohoungbogboanlyou@gmail.com>
2026-02-19 12:42:09 +0100

    chore: update dependencies and add ng-doc packages
    - Added @ng-doc/app, @ng-doc/builder, @ng-doc/core, and @ng-doc/ui-kit at version 21.0.0 to package.json.
    - Updated tsconfig.base.json to include path mappings for @ng-doc/generated and @ng-doc/generated/*.

--- apps/angular-web-ui-docs/src/app/app.config.ts
+++ apps/angular-web-ui-docs/src/app/app.config.ts
@@ -1,8 +1,11 @@
+import { provideNgDocApp, provideSearchEngine, NgDocDefaultSearchEngine, providePageSkeleton, NG_DOC_DEFAULT_PAGE_SKELETON, provideMainPageProcessor, NG_DOC_DEFAULT_PAGE_PROCESSORS } from "@ng-doc/app";
+import { NG_DOC_ROUTING, provideNgDocContext } from "@ng-doc/generated";
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from "@angular/common/http";
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
@@ -14,5 +17,13 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(appRoutes),
-  ],
+        provideHttpClient(withInterceptorsFromDi()),
+        provideRouter(NG_DOC_ROUTING, withInMemoryScrolling({scrollPositionRestoration: "enabled", anchorScrolling: "enabled"})),
+        provideHttpClient(withInterceptorsFromDi(), withFetch()),
+        provideNgDocContext(),
+        provideNgDocApp(),
+        provideSearchEngine(NgDocDefaultSearchEngine),
+        providePageSkeleton(NG_DOC_DEFAULT_PAGE_SKELETON),
+        provideMainPageProcessor(NG_DOC_DEFAULT_PAGE_PROCESSORS)
+    ],
 };
--- apps/angular-web-ui-docs/src/app/app.ts
+++ apps/angular-web-ui-docs/src/app/app.ts
@@ -1,9 +1,10 @@
+import { NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent } from "@ng-doc/app";
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule, NgDocRootComponent, NgDocNavbarComponent, NgDocSidebarComponent],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
